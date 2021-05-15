0cb72a8982a476a191d89e340fdbb9a568befeed
Vaibhav Singh <vsbb.singh@gmail.com>
2021-05-15 11:31:30 +0530

    Add method can handle 2 or less than 2 numbers

--- src/main/java/com/inn/Calculator/StringCalculator.java
+++ src/main/java/com/inn/Calculator/StringCalculator.java
@@ -3,7 +3,19 @@ package com.inn.Calculator;
 public class StringCalculator {
 	
 	public int add(String numbers) {
-		return 2;
+		if(numbers.length() == 0)
+			return 0;
+		else if(numbers.length() == 1)
+			return Integer.parseInt(numbers);
+		else {
+			String arr[] = numbers.split(",");
+			int sum = 0;
+			for(String number: arr)
+			{
+				sum += Integer.parseInt(number);
+			}
+			return sum;
+		}
 	}
 
 }
--- src/test/java/com/inn/StringCalculatorTest.java
+++ src/test/java/com/inn/StringCalculatorTest.java
@@ -10,7 +10,17 @@ public class StringCalculatorTest {
 	StringCalculator calculate = new StringCalculator();
 	
 	@Test
-	public void addTest() {
+	public void addEmptyStringTest() {
+		Assert.assertEquals(0,calculate.add(""));
+	}
+	
+	@Test
+	public void addOneNumberStringTest() {
+		Assert.assertEquals(2,calculate.add("2"));
+	}
+	
+	@Test
+	public void addTwoNumberStringTest() {
 		Assert.assertEquals(2,calculate.add("1,1"));
 	}
 
